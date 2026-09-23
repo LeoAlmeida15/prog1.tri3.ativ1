4f40b334ced4ba34e1bbfc299e4c9a36541f5b3e
LeoAlmeida15 <Leo.alm3ida@gmail.com>
2026-09-23 08:41:57 -0300

    Update db.ts

--- db.ts
+++ db.ts
@@ -1,21 +1,25 @@
 import { Database } from "bun:sqlite";
 
 const db = new Database("database.sqlite");
+
 const query = db.query(`
     CREATE TABLE IF NOT EXISTS users (
-        id              INTEGER PRIMARY KEY AUTOINCREMENT,
-        username        TEXT NOT NULL UNIQUE,
-        email           TEXT NOT NULL UNIQUE,
-        password_hash   TEXT NOT NULL
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL
     );
 
-    CREATE TABLE IF NOT EXISTS lista_exercicios (
-        id              INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome            VARCHAR, 
-        descricao       VARCHAR,
-        grupoMuscular   VARCHAR,
-        tipo            VARCHAR
+    CREATE TABLE IF NOT EXISTS receitas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR,
+        descricao VARCHAR,
+        ingredientes VARCHAR,
+        preparo VARCHAR,
+        categoria VARCHAR
+    )
 `);
+
 query.run();
 
-export { db }
+export { db };
