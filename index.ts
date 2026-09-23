b013e71045dca6dddbd9dfadf541f670669822e1
LeoAlmeida15 <Leo.alm3ida@gmail.com>
2026-09-23 08:33:39 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,23 +1,41 @@
 const srv = Bun.serve({
     port: 3000,
     routes: {
+        "/": {
+            GET: (a) => {
+                const url = new URL(a.url)
+                const search = url.searchParams
+                const nome = search.get("nome")
+                console.log(nome)
+                return new Response("oi " + nome)
+            },
+        },
+
+        "/pessoa/:id": {
+            GET: (req) => {
+            return new Response(`oi ${req.params.id}`)
+            }
+        },
+
         "/test": {
-            GET: () => {
+            GET: (req) => {
                 const url = new URL(req.url)
                 const search = url.searchParams
                 const nome = search.get("nome")
                 console.log(nome)
-                return new Response("_SEU_NOME get")
+                return new Response("LeoAlm3ida_GET")
             },
+
             POST: async (req) => {
                 const body = await req.body.text()
                 console.log(body)
-                return new Response("_SEU_NOME post")
+                return new Response("LeoAlm3ida_POST")
             },
-                PUT: () => new Response("_SEU_NOME put"),
-                DELETE: () => new Response("_SEU_NOME delete"),
-        }     
-    }
-    })
 
-    console.log(`Server running: ${srv.url}`)
+            PUT: () => new Response("LeoAlm3ida_PUT"),
+            DELETE: () => new Response("LeoAlm3ida_DELETE")
+        }
+    }
+})
+
+console.log(`Server running: ${srv.url}`)
